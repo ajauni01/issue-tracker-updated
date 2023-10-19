76bcea6c1afbc52414cf6584ba9cc10cf18d4ffb
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-19 03:28:37 -0500

    Implement client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,13 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod';
 import prisma from "@/prisma/client";
-
-
-//  use zod for data validation
-const createIssueSchema = z.object({
-    title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required.')
-})
+import { createIssueSchema } from "../../validationSchema";
 
 export async function POST(request: NextRequest){
  const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import { InfoCircledIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -7,16 +7,19 @@ import { useCallback, useMemo, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import {z} from 'zod';
 
-interface IssueForm{
-  title: string;
-  description: string;
-}
-
+// IssueForm is the type of the data that we are going to send to the server
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const {register, control, handleSubmit, formState:{errors}} = useForm<IssueForm>({
+    // integrate react hook form with zod for data validation
+    resolver:zodResolver(createIssueSchema)
+  });
   // console.log('register details', register('title'));
   // useState hook to handle the error
   const [error, setError] = useState("")
@@ -42,14 +45,20 @@ const NewIssuePage = () => {
         setError('An unexpected error occurred')
       }
     })}>
+      <div className='mb-2'>
       <TextField.Root>
         <TextField.Slot>
           <MagnifyingGlassIcon height='16' width='16' />
         </TextField.Slot>
         <TextField.Input placeholder='Title' {...register("title")} />
       </TextField.Root>
+      </div>
+      {/* display the error message related to the title */}
+      {errors.title && <Text color="red" as='p'>{errors.title.message}</Text>}
+    
       {/* TextArea to write the issue */}
       <Controller name="description" control={control}  render={({ field }) => <SimpleMDE placeholder='Please describe your issue' {...field} />} />
+      {errors.description && <Text color="red" as='p'>{errors.description.message}</Text>}
       <Button>Submit New Issue</Button>
     </form>
     </div>
@@ -62,3 +71,4 @@ export default NewIssuePage;
 // TODO: understand the reason of using angle brackets in the TextField component
 // TODO: understand the react hook form controller component
 // TODO: thoroughly understand the react hook form
+// TODO: understand the (type IssueForm = z.infer<typeof createIssueSchema>);
