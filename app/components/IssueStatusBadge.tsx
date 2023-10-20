e57b9f29469295566eedf262e93b3b8ae4185723
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-20 10:51:29 -0500

    Build the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,25 @@
+import { Status} from '@prisma/client'
+import { Badge } from '@radix-ui/themes';
+import React from 'react'
+
+// baddge status
+const statusMap: Record<
+Status, 
+{ label: string, color: 'red' | 'violet' | 'green' }> = {
+    OPEN: { label: 'Open', color: 'red' },
+    IN_PROGRESS: { label: 'In Progress', color: 'violet' },
+    CLOSED: { label: 'Closed', color: 'green' },
+  };
+
+const IssueStatusBadge = ({status}:{status:Status}) => {
+  return (
+<Badge color={statusMap[status].color}>
+    {statusMap[status].label}
+</Badge>
+
+    
+  )
+}
+
+export default IssueStatusBadge;
+// TODO: understand how the statusMap works
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 import prisma from '@/prisma/client';
-
+import IssueStatusBadge from '../components/IssueStatusBadge';
+  
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-
   return (
     <div>
       <div className='mb-5'>
@@ -16,8 +16,8 @@ const IssuesPage = async () => {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='md:table-cell'>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='md:table-cell'>Created</Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         {/* table body */}
@@ -25,12 +25,12 @@ const IssuesPage = async () => {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>{issue.title}
-                <div className='block md:hidden'>{issue.status}</div>
+                <div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
-                {issue.status}
+              <Table.Cell className='md:table-cell'>
+              <IssueStatusBadge status={issue.status}/>
                 </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className='md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
@@ -40,4 +40,4 @@ const IssuesPage = async () => {
 };
 
 export default IssuesPage;
-// TODO: Investgate why the md:table-cell class is not working
+// TODO: Investgate why the md:table-cell class is not working after adding the 'hidden' class to the div
