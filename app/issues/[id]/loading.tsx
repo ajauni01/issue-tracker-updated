6b1fa1355246584b13fbe1d26dde20ec21f9dc68
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-23 21:52:23 -0500

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div> Loading the issue detail page...</div>
+    <Box className='max-w-xl'>
+            <Skeleton/>
+            <Flex className='space-x-3 my-2'>
+              <Skeleton width="5rem"/>
+              <Skeleton width="8rem"/>
+            </Flex>
+            {/* add the pros class to improve the quality of the text written on the markdown editor */}
+            <Card className='prose mt-2'>
+              <Skeleton count={3}/>
+            </Card>
+    </Box>
   )
 }
 
