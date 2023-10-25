a5d8b5ac6940a41299138b7da768681ba358504a
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-24 20:51:32 -0500

    Dashboard-edited

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+// refactoring the import statements
+export { default as Link } from './Link';
+export { default as ErrorMessage} from './ErrorMessage';
+export {default as IssueStatusBadge} from './IssueStatusBadge';
+export  {Spinner} from './Spinner';
+     
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div>
-      Hello World!
+      DASHBOARD IS ON THE WAY ...
     </div>
   )
 }
