114ec244e0faacb03be45e85cfb6c11dbc9b3805
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-28 08:26:11 -0500

    Refactor: Organize import statements

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,9 +1,7 @@
+import { Skeleton } from '@/app/components';
 import { Table } from '@radix-ui/themes';
-import React from 'react'
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActions';
+import delay from 'delay';
 
 export const LoadingIssuePage = () => {
 const issues = [1,2,3,4,5,6];
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@radix-ui/themes'
 import delay from 'delay'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import {Skeleton} from '@/app/components';
 
 const LoadingNewIssuePage = () => {
   // simulate a delay of 2 seconds to show the loading skeleton
