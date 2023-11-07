78887b22f187225f3c881baacb3a6b607368dc7d
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-07 16:14:30 -0600

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 // use the 'use client' directive to make sure that the usePathname hook is only rendered on the client,
 "use client";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -24,36 +24,46 @@ const NavBar = () => {
   // usePathname is a hook that returns the current path of the page, and we use the current path to set the active link.
   const currentPath = usePathname();
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      {/* navbar links */}
-      <Link href="/">
-        {" "}
-        <AiFillBug />{" "}
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transation-colors": true,
-              })}
-              href={link.href}
-            >
-              {" "}
-              {link.label}{" "}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      {/* show the current user status */}
-      <Box>
-        {/* show the signout option if the user is already logged in */}
-        {status === "authenticated" && (<Link  href="api/auth/signout">Log out</Link>)}
-        {/* show the signin option if the user is not logged in */}
-        {status === "unauthenticated" && (<Link  href="api/auth/signin">Login</Link>)}
-      </Box>
+    <nav className="border-b mb-5 py-3 ">
+      <Container>
+      <Flex justify="between">
+        <Flex align="center" gap="3">
+          {/* navbar links */}
+          <Link href="/">
+            {" "}
+            <AiFillBug />{" "}
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classNames({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transation-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {" "}
+                  {link.label}{" "}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        {/* show the current user status */}
+        <Box>
+          {/* show the signout option if the user is already logged in */}
+          {status === "authenticated" && (
+            <Link href="api/auth/signout">Log out</Link>
+          )}
+          {/* show the signin option if the user is not logged in */}
+          {status === "unauthenticated" && (
+            <Link href="api/auth/signin">Login</Link>
+          )}
+        </Box>
+      </Flex>
+      </Container>
     </nav>
   );
 };
