1d92e0ae0f96107be364ea4b1f8882b64b3d141b
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-07 18:55:27 -0600

    Add a drop-down menu to show the current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,13 @@
 // use the 'use client' directive to make sure that the usePathname hook is only rendered on the client,
 "use client";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -26,43 +33,61 @@ const NavBar = () => {
   return (
     <nav className="border-b mb-5 py-3 ">
       <Container>
-      <Flex justify="between">
-        <Flex align="center" gap="3">
-          {/* navbar links */}
-          <Link href="/">
-            {" "}
-            <AiFillBug />{" "}
-          </Link>
-          <ul className="flex space-x-6">
-            {links.map((link) => (
-              <li key={link.href}>
-                <Link
-                  className={classNames({
-                    "text-zinc-900": link.href === currentPath,
-                    "text-zinc-500": link.href !== currentPath,
-                    "hover:text-zinc-800 transation-colors": true,
-                  })}
-                  href={link.href}
-                >
-                  {" "}
-                  {link.label}{" "}
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            {/* navbar links */}
+            <Link href="/">
+              {" "}
+              <AiFillBug />{" "}
+            </Link>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transation-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {" "}
+                    {link.label}{" "}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          {/* show the current user status */}
+          <Box>
+            {/* show the signout option if the user is already logged in */}
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="4"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="api/auth/signout">Sign out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {/* show the signin option if the user is not logged in */}
+            {status === "unauthenticated" && (
+              <Link href="api/auth/signin">Sign In</Link>
+            )}
+          </Box>
         </Flex>
-        {/* show the current user status */}
-        <Box>
-          {/* show the signout option if the user is already logged in */}
-          {status === "authenticated" && (
-            <Link href="api/auth/signout">Log out</Link>
-          )}
-          {/* show the signin option if the user is not logged in */}
-          {status === "unauthenticated" && (
-            <Link href="api/auth/signin">Login</Link>
-          )}
-        </Box>
-      </Flex>
       </Container>
     </nav>
   );
