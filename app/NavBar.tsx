c4d8a253a1a782c152c57c24b58b633a0a0e8c68
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-08 07:38:42 -0600

    Refactor the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,8 +15,26 @@ import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
-  const { status, data: session } = useSession();
-
+  return (
+    <nav className="border-b mb-5 py-3 ">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            {/* navbar links */}
+            <Link href="/">
+              {" "}
+              <AiFillBug />{" "}
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+// create a sub component for the navbar links to refactor the code
+const NavLinks = () => {
   /*
     In order to reduce the repetition of the href, label, 
     and tailwind class names, we can create an array of objects that contain the href and label
@@ -30,66 +48,60 @@ const NavBar = () => {
 
   // usePathname is a hook that returns the current path of the page, and we use the current path to set the active link.
   const currentPath = usePathname();
+
   return (
-    <nav className="border-b mb-5 py-3 ">
-      <Container>
-        <Flex justify="between">
-          <Flex align="center" gap="3">
-            {/* navbar links */}
-            <Link href="/">
-              {" "}
-              <AiFillBug />{" "}
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transation-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {" "}
-                    {link.label}{" "}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          {/* show the current user status */}
-          <Box>
-            {/* show the signout option if the user is already logged in */}
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="4"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="api/auth/signout">Sign out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {/* show the signin option if the user is not logged in */}
-            {status === "unauthenticated" && (
-              <Link href="api/auth/signin">Sign In</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {" "}
+            {link.label}{" "}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
+
+// create a sub component for the user or dropdown menu to refactor the code
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  //   return null if the session is loading
+  if (status === "loading") return null;
+  /* show the signin option if the user is not logged in */
+  if (status === "unauthenticated")
+    return <Link className="nav-link" href="api/auth/signin">Sign In</Link>;
+  return (
+    <Box>
+      {/* show the signout option in a drop-down menu if the user is already logged in */}
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="4"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="api/auth/signout">Sign out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
 export default NavBar;
