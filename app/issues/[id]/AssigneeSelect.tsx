24b64461dfaa9fb6f7490e7e5d9f912466b7e2cc
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-14 21:14:48 -0600

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,31 +1,38 @@
 "use client";
 
+import Skeleton from "@/app/components/Skeleton";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
-  // useState hook to store the current state of the users
-  const [users, setUsers] = useState<User[]>([]);
-  // useEffect is a hook that lets you perform side effects in function components
-  useEffect(() => {
-    // fetchUsers is an async function that fetches the users from the database
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      //   setUsers is a function that updates the users state
-      setUsers(data);
-    };
-    // call the fetchUsers function to populate the users state
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    // queryKey is a unique identifier for the query
+    queryKey: ["users"],
+    // queryFn is a function that returns the data
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    // staleTime is the time in milliseconds before the data is considered stale // we are using it for caching
+    staleTime: 60 * 1000,
+    // retry is the number of times to retry the query if it fails
+    retry: 3,
+  });
+  //   return the loading skeleton if the data is still loading
+  if (isLoading) return <Skeleton />;
+  //   return null if there is still error after retrying 3 times
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
@@ -37,3 +44,5 @@ const AssigneeSelect = () => {
 };
 
 export default AssigneeSelect;
+
+// TODO: deep dive into the queryKey and queryFn
