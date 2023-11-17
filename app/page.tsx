bc4f7c317e472122edec46d63adfbca38ee52505
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.ad.wsc.edu>
2023-11-17 12:21:42 -0600

    Build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import LatestIssues from "./LatestIssues";
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const close = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <LatestIssues/>
+    <IssueSummary open={open} inProgress={inProgress} closed={3} />
+    // <LatestIssues/>
   );
 }
 
