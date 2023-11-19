55c6338b1c0bfe9f51afacdc638e6260365abf00
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-19 14:17:35 -0600

    Layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
+import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -9,8 +11,13 @@ export default async function Home() {
   });
   const close = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <IssueChart open={open} inProgress={inProgress} closed={3} />
-    // <LatestIssues/>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={close} />
+        <IssueChart open={open} inProgress={inProgress} closed={close} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
   );
 }
 
